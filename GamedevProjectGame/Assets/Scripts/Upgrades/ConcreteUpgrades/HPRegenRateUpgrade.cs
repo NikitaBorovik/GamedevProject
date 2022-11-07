c1e7295fb9dbe2e9c2cae2a43061246dabfa19b3
Fourier2718281828 <non-linear@ukr.net>
2022-11-08 00:28:23 +0200

    +added some specific upgrades

---
+++ GamedevProjectGame/Assets/Scripts/Upgrades/ConcreteUpgrades/HPRegenRateUpgrade.cs
@@ -0,0 +1,25 @@
+﻿using App.World.Entity.Player.PlayerComponents;
+using System;
+using UnityEngine;
+
+namespace App.Upgrades.ConcreteUpgrades
+{
+    [CreateAssetMenu(fileName = "HPRegenRateUp", menuName = "Scriptable Objects/Upgrades/HPRegenRateUpgrade")]
+    public class HPRegenRateUpgrade : ScriptableObject, IUpgrade
+    {
+        [Min(0.0f)]
+        [SerializeField] private float cooldownMultiplier;
+
+        public void Enable(Player upgradable)
+        {
+            upgradable.Weapon.Cooldown *= cooldownMultiplier;
+        }
+
+        public void Update(Player upgradable) { }
+
+        public void Disable(Player upgradable)
+        {
+            upgradable.Weapon.Cooldown /= cooldownMultiplier;
+        }
+    }
+}
---
+++ GamedevProjectGame/Assets/Scripts/Upgrades/ConcreteUpgrades/WeaponCooldownUpgrade.cs
@@ -0,0 +1,25 @@
+﻿using App.World.Entity.Player.PlayerComponents;
+using System;
+using UnityEngine;
+
+namespace App.Upgrades.ConcreteUpgrades
+{
+    [CreateAssetMenu(fileName = "WpnCooldownUp", menuName = "Scriptable Objects/Upgrades/WeaponCooldownUpgrade")]
+    public class WeaponCooldownUpgrade : ScriptableObject, IUpgrade
+    {
+        [Range(0f, 1f)]
+        [SerializeField] private float cooldownMultiplier;
+
+        public void Enable(Player upgradable)
+        {
+            upgradable.Weapon.Cooldown *= cooldownMultiplier;
+        }
+
+        public void Update(Player upgradable) {}
+
+        public void Disable(Player upgradable)
+        {
+            upgradable.Weapon.Cooldown /= cooldownMultiplier;
+        }
+    }
+}
