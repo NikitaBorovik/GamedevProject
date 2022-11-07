4bdaea9fb0dc842ab9db7686bf7ec9ccc395ec1f
Fourier2718281828 <non-linear@ukr.net>
2022-11-08 00:22:05 +0200

    +added UpgradeManager

---
+++ GamedevProjectGame/Assets/Scripts/Upgrades/UpgradeManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace App.Upgrades
+{
+    public class UpgradeManager : MonoBehaviour
+    {
+        #region Fields
+        private List<IUpgrade> upgrades;
+        private IUpgradable upgradableEntity;
+        #endregion
+
+        #region MonoBehaviour Methods
+        private void Awake()
+        {
+            upgradableEntity = GetComponent<IUpgradable>();
+        }
+
+        private void Update()
+        {
+            UpdateAll();
+        }
+        #endregion
+
+        #region Custom Methods
+        public void EnableAll() => upgrades.ForEach(u => upgradableEntity.EnableUpgrade(u));
+
+        public void UpdateAll() => upgrades.ForEach(u => upgradableEntity.UpdateUpgrade(u));
+
+        public void DisableAll() => upgrades.ForEach(u => upgradableEntity.DisableUpgrade(u));
+        #endregion
+    }
+}
