330969c6e70dee8239f3c3738f602ac4882ed858
Нікіта Боровік <nikita.borovik@ukma.edu.ua>
2022-11-08 21:48:30 +0200

    + added base upgrade abstract class

---
+++ GamedevProjectGame/Assets/Scripts/Upgrades/ConcreteUpgrades/BaseUpgrade.cs
@@ -0,0 +1,17 @@
+using App.Upgrades;
+using App.World.Entity.Player.PlayerComponents;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class BaseUpgrade : ScriptableObject, IUpgrade
+{
+    public abstract void Disable(Player upgradable);
+
+
+    public abstract void Enable(Player upgradable);
+
+
+    public abstract void UpdateUpgrade(Player upgradable);
+    
+}
