e43ea0ce1f5dc023134b90e585c34b5a22d15207
Ivan Blyzniuk <ivan.blyzniuk@ukma.edu.ua>
2022-12-14 23:59:31 +0200

    ~Reworked waves

--- GamedevProjectGame/Assets/Scripts/Systems/Wave/WaveSystem.cs
+++ GamedevProjectGame/Assets/Scripts/Systems/Wave/WaveSystem.cs
@@ -14,26 +14,47 @@ namespace App.Systems.Wave
         private GameStatesSystem gameStatesSystem;
         private int enemiesAlive = 0;
         private int dangerLevelLeft;
-        private int totalDangerLevel = 200;
+        private int totalDangerLevel = 300;
         private List<BaseEnemy> allowedEnemies = new List<BaseEnemy>();
+        private Dictionary<BaseEnemy, float> enemyWeights;
+        private float currentTotalEnemyWeight;
 
         [SerializeField]
-        private float minTimeBetweenSpawns;
+        private float minTimeBetweenSubwaves;
         [SerializeField]
-        private float maxTimeBetweenSpawns;
+        private float maxTimeBetweenSubwaves;
+        [SerializeField]
+        private float subWaveDangerPercentage;
+        [SerializeField]
+        private float nextWaveDangerLevelMultiplier;
         [SerializeField]
         private List<BaseEnemy> enemies;
 
         public void StartWave()
         {
-            allowedEnemies = enemies.FindAll(e => e.EnemyData.firstSpawningWave >= waveNum);
+            allowedEnemies = enemies.FindAll(e => e.EnemyData.firstSpawningWave <= waveNum);
+            currentTotalEnemyWeight = 0;
+            foreach(BaseEnemy enemy in allowedEnemies)
+            {
+                currentTotalEnemyWeight += enemyWeights[enemy];
+            }
             StartCoroutine(Wave());
         }
 
         public void Init(EnemySpawningSystem enemySpawningSystem, GameStatesSystem gameStatesSystem)
         {
             this.enemySpawningSystem = enemySpawningSystem;
-            this.gameStatesSystem = gameStatesSystem;   
+            this.gameStatesSystem = gameStatesSystem;
+            CalculateEnemyWeights();
+        }
+
+        private void CalculateEnemyWeights()
+        {
+            enemyWeights = new Dictionary<BaseEnemy, float>();
+            foreach(BaseEnemy enemy in enemies)
+            {
+                enemyWeights.Add(enemy, 1.0f / enemy.EnemyData.dangerLevel);
+            }
         }
 
         public IEnumerator Wave()
@@ -42,28 +63,39 @@ namespace App.Systems.Wave
             while(dangerLevelLeft > 0)
             {
                 SpawnSubWave();
-                yield return new WaitForSeconds(Random.Range(minTimeBetweenSpawns,maxTimeBetweenSpawns));
+                yield return new WaitForSeconds(Random.Range(minTimeBetweenSubwaves, maxTimeBetweenSubwaves));
             }
         }
 
         private void SpawnSubWave()
         {
-            int dangerDiff = (int)(totalDangerLevel * 0.1);
+            int dangerDiff = (int)(totalDangerLevel * subWaveDangerPercentage);
             int startingDangerLevel = dangerLevelLeft;
             while(dangerLevelLeft > startingDangerLevel - dangerDiff)
             {
-                int enemyIndex = Random.Range(0, allowedEnemies.Count);
-                Debug.Log(allowedEnemies.Count);
-                BaseEnemy randomEnemy = allowedEnemies[enemyIndex];
+                //int enemyIndex = Random.Range(0, allowedEnemies.Count);
+                BaseEnemy randomEnemy = getRandomEnemy();// allowedEnemies[enemyIndex];
                 enemySpawningSystem.SpawnEnemy(randomEnemy.gameObject);
                 dangerLevelLeft -= randomEnemy.EnemyData.dangerLevel;
                 enemiesAlive++;
             }
         }
 
+        private BaseEnemy getRandomEnemy()
+        {
+            float randomWeight = Random.value * currentTotalEnemyWeight;
+            foreach(BaseEnemy enemy in allowedEnemies)
+            {
+                if (enemyWeights[enemy] >= randomWeight)
+                    return enemy;
+                randomWeight -= enemyWeights[enemy];
+            }
+            return allowedEnemies[allowedEnemies.Count - 1];
+        }
+
         private void CalculateNextDangerLevel()
         {
-            totalDangerLevel = (int)(totalDangerLevel * 1.2);
+            totalDangerLevel = (int)(totalDangerLevel * nextWaveDangerLevelMultiplier);
         }
 
         public void ReportKilled(string enemyType)
