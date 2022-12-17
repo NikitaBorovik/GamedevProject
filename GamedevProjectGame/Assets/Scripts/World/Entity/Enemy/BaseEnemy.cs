f91b29d10cca3f7cfb530c5fb273aaf015f7763c
Ivan Blyzniuk <ivan.blyzniuk@ukma.edu.ua>
2022-12-17 22:42:17 +0200

    ~Cleaned up BaseEnemy after fixes

--- GamedevProjectGame/Assets/Scripts/World/Entity/Enemy/BaseEnemy.cs
+++ GamedevProjectGame/Assets/Scripts/World/Entity/Enemy/BaseEnemy.cs
@@ -128,15 +128,10 @@ namespace World.Entity.Enemy
         {
             objectPool = pool;
             gameObject.SetActive(true);
-            animator.Rebind();
         }
 
         public void ReturnToPool()
         {
-            //animator.CrossFade("Empty State", 0f);
-            //animator.Update(0f);
-            //animator.Update(0f);
-            //animator.Update(0f);
             gameObject.SetActive(false);
         }
 
