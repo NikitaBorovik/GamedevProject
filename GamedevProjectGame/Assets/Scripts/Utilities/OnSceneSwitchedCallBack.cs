3ce84408be288e5263ce2b27dad2bc88f824638a
Fourier2718281828 <non-linear@ukr.net>
2022-12-20 03:40:18 +0200

    +loading screen

--- GamedevProjectGame/Assets/Scripts/Utilities/OnSceneSwitchedCallBack.cs
+++ GamedevProjectGame/Assets/Scripts/Utilities/OnSceneSwitchedCallBack.cs
@@ -1,6 +1,25 @@
 using UnityEngine;
 
-public class OnSceneSwitchedCallBack : MonoBehaviour
+namespace App.Utilities
 {
-    private void Start() => SceneSwitcher.SwitchToNextLoadedScene();
+    public class OnSceneSwitchedCallBack : MonoBehaviour
+    {
+        [SerializeField] private float minimalLoadingTime;
+        private float currTime;
+
+        private void Awake()
+        {
+            currTime = 0f;
+        }
+
+        private void FixedUpdate()
+        {
+            currTime += Time.fixedDeltaTime;
+
+            if(currTime > minimalLoadingTime)
+            {
+                SceneSwitcher.SwitchToNextLoadedScene?.Invoke();
+            }
+        }
+    }
 }
--- GamedevProjectGame/Assets/Scripts/Utilities/SceneSwitcher.cs
+++ GamedevProjectGame/Assets/Scripts/Utilities/SceneSwitcher.cs
@@ -1,39 +1,41 @@
 using System;
 using System.Linq;
 using UnityEditor;
-using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class SceneSwitcher : MonoBehaviour
+namespace App.Utilities
 {
-    public delegate void OnSceneSwitched();
-    public static OnSceneSwitched SwitchToNextLoadedScene;
-    private static readonly string LoadingSceneName = "Loading";
-
-    public static void SwitchToSceneLoadingScreen(string sceneName)
+    public static class SceneSwitcher
     {
-        SwitchToNextLoadedScene = () => SwitchToScene(sceneName);
-        SwitchToScene(LoadingSceneName);
+        public delegate void OnSceneSwitched();
+        public static Action SwitchToNextLoadedScene;
+        private static readonly string LoadingSceneName = "Loading";
+
+        public static void SwitchToSceneLoadingScreen(string sceneName)
+        {
+            SwitchToNextLoadedScene = () => SwitchToScene(sceneName);
+            SwitchToScene(LoadingSceneName);
+        }
+
+        public static void SwitchToScene(string sceneName)
+        {
+            if (!DoesSceneExist(sceneName))
+                throw new ArgumentException($"The scene {sceneName} does not exist in the build settings" +
+                    $" or is not enabled. Add the one to build settings or enable.");
+
+            if (IsRunning(sceneName))
+                throw new InvalidOperationException($"Cannot switch to scene {sceneName} that is currently running.");
+
+            SceneManager.LoadScene(sceneName);
+        }
+
+        public static bool DoesSceneExist(string sceneName)
+            => EditorBuildSettings.scenes.Any
+            (
+                scene => scene.enabled && scene.path.Contains("/" + sceneName + ".unity")
+            );
+
+        public static bool IsRunning(string sceneName)
+           => SceneManager.GetActiveScene().name == sceneName;
     }
-
-    public static void SwitchToScene(string sceneName) 
-    {
-        if (!DoesSceneExist(sceneName))
-            throw new ArgumentException ($"The scene {sceneName} does not exist in the build settings" +
-                $" or is not enabled. Add the one to build settings or enable.");
-
-        if (IsRunning(sceneName))
-            throw new InvalidOperationException($"Cannot switch to scene {sceneName} that is currently running.");
-       
-        SceneManager.LoadScene(sceneName);
-    } 
-
-    public static bool DoesSceneExist(string sceneName)
-        => EditorBuildSettings.scenes.Any
-        (
-            scene => scene.enabled && scene.path.Contains("/" + sceneName + ".unity")
-        );
-
-    public static bool IsRunning(string sceneName)
-       => SceneManager.GetActiveScene().name == sceneName;
 }
